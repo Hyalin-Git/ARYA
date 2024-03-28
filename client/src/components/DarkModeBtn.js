bbe2d1d4061070a21c6b121f506de0bd03e4532a
Hyalin-Git <nicolas.tombal01@gmail.com>
2024-03-28 16:09:19 +0100

    dark/light btn animation done

--- client/src/components/DarkModeBtn.js
+++ client/src/components/DarkModeBtn.js
@@ -1,44 +1,46 @@
 import { DarkModeContext } from "@/context/darkMode";
 import Image from "next/image";
 import styles from "@/styles/components/darkModeBtn.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 export default function DarkModeBtn() {
 	const { isDarkMode, setIsDarkMode } = useContext(DarkModeContext);
-
+	const [image, setImage] = useState("images/icons/moon_icon.svg");
 	function switchMode(e) {
 		e.preventDefault();
-		const checkbox = document.getElementById("darkMode");
-		if (!checkbox.checked) {
-			checkbox.checked = true;
-			document.getElementsByTagName("body")[0].setAttribute("data-dark", true);
-			setIsDarkMode(true);
-		} else {
-			checkbox.checked = false;
-			document.getElementsByTagName("body")[0].setAttribute("data-dark", false);
-			setIsDarkMode(false);
-		}
+
+		const switchBtn = document.getElementById("switch");
+		switchBtn.classList.add(styles.anim);
+		let timeout;
+		clearTimeout(timeout);
+
+		timeout = setTimeout(() => {
+			if (!isDarkMode) {
+				setImage("images/icons/sun_icon.svg");
+			} else {
+				setImage("images/icons/moon_icon.svg");
+			}
+		}, 250);
+
+		switchBtn.addEventListener("animationend", () => {
+			if (!isDarkMode) {
+				document
+					.getElementsByTagName("body")[0]
+					.setAttribute("data-dark", true);
+				setIsDarkMode(true);
+			} else {
+				document
+					.getElementsByTagName("body")[0]
+					.setAttribute("data-dark", false);
+				setIsDarkMode(false);
+			}
+			switchBtn.classList.remove(styles.anim);
+		});
 	}
 
 	return (
 		<div className={styles.container}>
-			<input hidden type="checkbox" name="darkMode" id="darkMode" />
-			<div onClick={switchMode} data-dark={isDarkMode} className={styles.btn}>
-				{isDarkMode && (
-					<Image
-						src={"/images/icons/sun_icon.svg"}
-						alt="icon"
-						width={20}
-						height={20}
-					/>
-				)}
-				{!isDarkMode && (
-					<Image
-						src={"/images/icons/moon_icon.svg"}
-						alt="icon"
-						width={20}
-						height={20}
-					/>
-				)}
+			<div onClick={switchMode} className={styles.btn} id="switch">
+				<Image src={`/${image}`} alt="icon" width={20} height={20} />
 			</div>
 		</div>
 	);
