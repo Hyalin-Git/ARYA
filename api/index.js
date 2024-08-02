fe057eb3b5120237b9bc6e0069dfcb3dcee1e457
Hyalin-Git <nicolas.tombal01@gmail.com>
2024-08-02 20:10:53 +0200

    getting closer

--- api/index.js
+++ api/index.js
@@ -121,7 +121,7 @@ const server = app.listen(process.env.PORT, (err) => {
 
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "*",
 		methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 		credentials: true,
 	},
