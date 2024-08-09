f4528bdad8594eaf255e550c928f99320bb7aa6e
Nicolas <nicolas.tombal01@gmail.com>
2024-08-09 14:15:08 +0200

    new icon for follow

--- client/src/components/social/FollowButton.js
+++ client/src/components/social/FollowButton.js
@@ -1,12 +1,13 @@
 "use client";
-import Image from "next/image";
 import { follow } from "@/api/user/user";
 import { AuthContext } from "@/context/auth";
 import { useContext } from "react";
 import { mutate } from "swr";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function FollowButton({ idToFollow, limit }) {
-	const { user, setUser, uid } = useContext(AuthContext);
+	const { setUser, uid } = useContext(AuthContext);
 
 	async function handleFollow(e) {
 		e.preventDefault();
@@ -16,13 +17,13 @@ export default function FollowButton({ idToFollow, limit }) {
 
 	return (
 		<>
-			<Image
+			<FontAwesomeIcon
+				icon={faUserPlus}
+				title="Suivre cet utilisateur"
 				onClick={handleFollow}
-				src={"./images/icons/check_icon.svg"}
-				alt="icon"
-				width={25}
-				height={25}
 				style={{
+					width: "25px",
+					height: "25px",
 					filter:
 						"invert(19%) sepia(98%) saturate(2685%) hue-rotate(258deg) brightness(91%) contrast(97%)",
 					cursor: "pointer",
